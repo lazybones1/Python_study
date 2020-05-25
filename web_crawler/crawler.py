bb2f06680b09eebfa0c00b49043f0c881795a9da
lazybones1 <lazybones1@naver.com>
2020-05-26 00:02:06 +0900

    local 실행, loop 2회부터 error

--- web_crawler/crawler.py
+++ web_crawler/crawler.py
@@ -9,7 +9,7 @@ link = 'https://play.google.com/store/apps/details?id=com.miso&hl=ko&showAllRevi
 scroll_cnt = 10 #원하는 스크롤 수
 
 #크롬드라이버 다운 :https://sites.google.com/a/chromium.org/chromedriver/home
-driver = webdriver.Chrome() #크롬 드라이버 경로
+driver = webdriver.Chrome('./chromedriver') #크롬 드라이버 경로
 driver.get(link) #크롬드라이버에게 링크로 접속하라고 명령
 
 os.makedirs('result', exist_ok=True) #새로운 폴더를 만든다
@@ -37,8 +37,8 @@ df = pd.DataFrame(columns=['name', 'ratings', 'date', 'helpful', 'comment', 'dev
 #리뷰 데이터 찾기
 for review in reviews:
   #html parser를 이용해서 parsing
-  #ger_attribute('innerHTML') : Html요소를 text형태로 가져온다
-  soup = BeautifulSoup(review.ger_attribute('innerHTML'), 'html.parser')
+  #get_attribute('innerHTML') : Html요소를 text형태로 가져온다
+  soup = BeautifulSoup(review.get_attribute('innerHTML'), 'html.parser')
 
   #리뷰 작성자 찾기
   #soup.find : 파싱된 html에서 요소를 찾기
@@ -48,7 +48,7 @@ for review in reviews:
   ratings = int(soup.find('div', role='img').get('aria-label').replace('별표 5개 만점에', '').replace('개를 받았습니다.', '').strip())
 
   #리뷰 날짜 찾기
-  date = soup.find(class_='p2Tk0b').text
+  date = soup.find(class_='p2TkOb').text
   date = datetime.strptime(date, '%Y년 %m월 %d일')
   date = date.strftime('%Y-%m-%d')
 
@@ -80,7 +80,7 @@ for review in reviews:
   }, ignore_index=True)
 
   #scv 파일로 데이터프레임 저장
-  filename = datetime.now().strfrime('result/%Y-%m-%d_%H-%M-%S.csv')
+  filename = datetime.now().strftime('result/%Y-%m-%d_%H-%M-%S.csv')
   #df.to_csv() csv파일로 저장
   df.to_csv(filename, encoding='utf-8-sig', index=False)
   driver.stop_client() #selenium클라이언트 종료
