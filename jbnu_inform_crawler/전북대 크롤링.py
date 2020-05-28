dcd487e3ff29ac5fa416a5b1b6d4793c469b1973
lazybones1 <lazybones1@naver.com>
2020-05-28 22:31:54 +0900

    extends page range

--- jbnu_inform_crawler/전북대 크롤링.py
+++ jbnu_inform_crawler/전북대 크롤링.py
@@ -7,33 +7,34 @@ from datetime import datetime
 driver = webdriver.Chrome('./chromedriver')
 driver.implicitly_wait(1)
 
-page = 1
-url = f"https://www.jbnu.ac.kr/kor/?menuID=139&pno={page}"
-driver.get(url)
-
-try:
-    list = driver.find_elements_by_xpath('//*[@class="page_list"]/table[@class="ta_bo"]/tbody/tr')
-    print("find %d page list" %len(list))
-except:
-    print("xpath err")
-    driver.stop_client()
-    dirver.quit()
-    sys.exit()
-
+page_cnt = 5;
 df = pd.DataFrame(columns=['title', 'link', 'date'])
 
-for i in list:
-    soup = BeautifulSoup(i.get_attribute('innerHTML'), 'html.parser')
+for page in range(1, page_cnt+1):
+    url = f"https://www.jbnu.ac.kr/kor/?menuID=139&pno={page}"
+    driver.get(url)
+    try:
+        list = driver.find_elements_by_xpath('//*[@class="page_list"]/table[@class="ta_bo"]/tbody/tr')
+        print("find %d page list" %len(list))
+    except:
+        print("xpath err")
+        driver.stop_client()
+        dirver.quit()
+        sys.exit()
 
-    title = soup.find('td', {'class':'left'}).find('a')["title"]
-    link = soup.find('td', {'class':'left'}).find('a')["href"]
-    date = soup.findAll('td', {'class':'mview'}, limit=2)[1].text
+    for i in list:
+        soup = BeautifulSoup(i.get_attribute('innerHTML'), 'html.parser')
 
-    df = df.append({
-    'title': title,
-    'link': link,
-    'date': date
-    }, ignore_index=True)
+        title = soup.find('td', {'class':'left'}).find('a')["title"]
+        link = soup.find('td', {'class':'left'}).find('a')["href"]
+        date = soup.findAll('td', {'class':'mview'}, limit=2)[1].text
+
+        df = df.append({
+        'title': title,
+        'link': link,
+        'date': date
+        }, ignore_index=True)
+    print(page)
 
 os.makedirs('result', exist_ok=True)
 filename = datetime.now().strftime('result/%Y-%m-%d_%H-%M-%S.csv')
